0a25f6a37525cc67f8f7827ece9434cf90854b9d
Aravix <49846328+aravikusu@users.noreply.github.com>
2026-05-06 14:18:58 +0200

    change telescope grep search name cause it annoyed me

--- lua/config/aravix.lua
+++ lua/config/aravix.lua
@@ -1,12 +1,21 @@
+-- enable build-in undotree on startup
 vim.cmd.packadd("nvim.undotree")
+
+-- show spaces, tabs, leading whitespace
 vim.o.list = true
 vim.o.listchars = 'tab:» ,lead:•,trail:•'
 vim.g.mapleader = " "
+
+-- tab spacing
 vim.opt.tabstop = 4
 vim.opt.softtabstop= 4
 vim.opt.shiftwidth = 4
+
+-- hide --INSERT-- etc, since lunaline handles it
 vim.opt.showmode = false
 vim.expandtab = true
+
+-- keybinds
 vim.keymap.set("n", "<A-Left>", vim.cmd.BufferLineCyclePrev, { desc = "Bufeline tab left"} )
 vim.keymap.set("n", "<A-Right>", vim.cmd.BufferLineCycleNext, { desc = "Bufferline tab right"} )
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Go back" })
@@ -14,6 +23,7 @@ vim.keymap.set("n", "<leader>u", vim.cmd.Undotree, { desc = "Open Undotree" })
 vim.keymap.set("n", "<leader>e", vim.cmd.Neotree, { desc = "Open Neo-tree" })
 vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostics" })
 
+-- godot server
 local projectfile = vim.fn.getcwd() .. '/project.godot'
 if vim.fn.filereadable(projectfile) == 1 then
 	vim.fn.serverstart './godothost'
